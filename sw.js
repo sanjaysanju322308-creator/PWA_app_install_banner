de20d340e6d3128b6e3c387c00f69ee47da8fe74
sanjaysanju322308-creator <sanjaysanju@gmail.com>
2026-02-24 16:52:53 +0530

    second commit

--- sw.js
+++ sw.js
@@ -1,5 +1,12 @@
 self.addEventListener("install", function (event) {
     console.log("Installed", event);
+    event.waitUntil(
+        caches.open("static").then((cache) => {
+            cache.add("/js/jquery-3.5.1.min.js");
+            cache.add("/js/script.js");
+            cache.add("/");
+        })
+    );
 });
 
 self.addEventListener("activate", function (event) {
@@ -8,6 +15,14 @@ self.addEventListener("activate", function (event) {
 });
 
 self.addEventListener("fetch", function (event) {
-    console.log("Service Worker fetch", event);
-    event.respondWith(fetch (event.request))
+    // console.log("Service Worker fetch", event);
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            if (response) {
+                return response;
+            } else {
+                return fetch(event.request);
+            }
+        })
+    )
 })
