935ee3394fdf7b0bee301303b30cf274fe9559fd
sanjaysanju322308-creator <sanjaysanju@gmail.com>
2026-02-27 16:30:26 +0530

    second commit

--- sw.js
+++ sw.js
@@ -1,17 +1,48 @@
+const STATIC_CACHE = "static-v4";
+const DYNAMIC_CACHE = "dynamic-v4";
+
 self.addEventListener("install", function (event) {
     console.log("Installed", event);
     event.waitUntil(
-        caches.open("static").then((cache) => {
-            cache.add("/js/jquery-3.5.1.min.js");
-            cache.add("/js/script.js");
-            cache.add("/");
+        caches.open("static-v1").then((cache) => {
+            cache.addAll([
+                "/",
+                "index.html",
+                "/manifest.json",
+                "/js/script.js",
+                "/js/jquery-3.5.1.min.js",
+
+                "/css/style.css",
+
+                "/images/logo.svg",
+                "/images/search.svg",
+
+                "/fonts/product_sans_bold-webfont.woff2",
+                "/fonts/product_sans_bold-webfont.woff",
+                "/fonts/product_sans_regular-webfont.woff2",
+                "/fonts/product_sans_regular-webfont.woff",
+
+                "https://traveller.talrop.works/api/v1/places/categories/",
+                "https://traveller.talrop.works/api/v1/places/"
+            ])
         })
     );
 });
 
 self.addEventListener("activate", function (event) {
     console.log("Activated", event);
-    return self.clients.claim();
+    event.waitUntil(
+        caches.keys().then((keyList) => {
+            return Promise.all(
+                keyList.map((key) =>{
+                    if (key !== "STATIC_CACHE" && key !== "DYNAMIC_CACHE") {
+                        console.log(key);
+                        return caches.delete(key);
+                    }
+                })
+            );
+        })
+    );
 });
 
 self.addEventListener("fetch", function (event) {
@@ -21,7 +52,13 @@ self.addEventListener("fetch", function (event) {
             if (response) {
                 return response;
             } else {
-                return fetch(event.request);
+                return fetch(event.request).then((res) => {
+                    return caches.open("DYNAMIC_CACHE").then((cache) => {
+                        cache.put(event.request.url,res.clone());
+                        return res;
+                    })
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                
+                })
             }
         })
     )
